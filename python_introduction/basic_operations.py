cd6b375594f385561cfc2f9bc5d0e6340e86dcfb
ahmed elbahy <exbahy@gmail.com>
2025-07-12 19:16:29 +0300

    Add script for basic arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# 1. Assign values to the variables
+number1 = 10
+number2 = 5
+
+# 2. Perform the calculations and store the results in new variables
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+
+# 3. Print the results in the required format using f-strings
+print(f"Addition of {number1} and {number2} is {sum_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
